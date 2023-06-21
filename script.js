8a9d46be76177213acc47b1f2219e77036fbc593
KabongeHajarah <kabongehajarah@gmail.com>
2023-06-21 19:57:49 +0400

    commits

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-// try to develop a program which calculate measure of central tendency of a sample(mean, median, mode) and measure of variability(range, variance, standard deviation). In addition to those measures find the min, max, count, percentile, and frequency distribution of the sample. You can create a class called Statistics and create all the functions which do statistical calculations as method for the Statistics class. 
+// try to develop a program which
+// calculate measure of central tendency of a 
+///sample(mean, median, mode) and
+// measure of variability(range, variance, standard deviation). 
+//In addition to those measures find the min, max, count, percentile, 
+//and frequency distribution of the sample.
+// You can create a class called Statistics and
+// create all the functions which do statistical calculations as method for the Statistics class. 
 
 class Statistics {
   constructor(data) {
