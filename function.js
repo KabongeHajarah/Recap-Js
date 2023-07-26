dcbee008f11e25445985da860bd2402589061eab
KabongeHajarah <kabongehajarah@gmail.com>
2023-07-26 23:58:30 +0400

    Clean and pretty Code

--- function.js
+++ function.js
@@ -1,25 +1,5 @@
-//creating an object with a constructor function
-
-function Person(firstName,lastName,dob){
-    this.firstName=firstName;
-    this.lastName=lastName;
-    this.dob=new Date(dob);
-    this.getBirthYear= function(){
-        return this.dob.getFullYear();
-    }
-    this.getFullName=function(){
-        return  `${this.firstName} ${this.lastName}`
-    }
-}
-Person.prototype.getFullName=function(){
-    return  `${this.firstName} ${this.lastName}`
-}
-Person.prototype.getBirthYear= function(){
-    return this.dob.getFullYear();
-}
-
 //Class
-class Persons{
+class Person{
     constructor (firstName,lastName,dob){
         this.firstName=firstName;
         this.lastName=lastName;
