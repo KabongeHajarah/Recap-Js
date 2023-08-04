2fba112d6cdfec7125f62411cc3f6ca511106fdd
KabongeHajarah <kabongehajarah@gmail.com>
2023-08-05 00:15:49 +0400

    Exporting and Importing a Module

--- function.js
+++ function.js
@@ -21,4 +21,17 @@ console.log(person1.dob.getFullYear());
 console.log(person1.getBirthYear());
 console.log(person1.getFullName());
 
+//Import from new.js
+//// to import the doSomeMath from the math.js with or without extension
+import doSomeMath from './new.js'
 
+//// to import the other modules
+// since these modules were not exported as default we have to desctructure
+import { addTwo,multiply,subtract } from './new.js';
+
+import * as everything from './new.js' //// to import everything remaining
+console.log(addTwo(5, 5))
+console.log(doSomeMath.addTwo(5, 5))
+console.log(everything)
+console.log(multiply(1,2));
+console.log(10,20);
--- new.js
+++ new.js
@@ -1,14 +1,13 @@
-// JSX syntax
-// we don't need to use quotes with JSX
+//Modules
 
-const jsxElement = <h1>I am a JSX element</h1>
-const welcome = <h1>Welcome to 30 Days of React Challenge</h1>
-const data = <small>Oct 2, 2020</small>
+  export const addTwo=(a,b)=>(a+b);
+  export const multiply = (a, b) => a * b;
+export const subtract = (a, b) => a - b;
 
-const header = (
-    <header>
-      <h1>Welcome to 30 Days Of React</h1>
-      <h2>Getting Started React</h2>
-      <h3>JavaScript Library</h3>
-    </header>
-  )
+export default (function doSomeMath(){
+return {
+  addTwo,
+  multiply,
+  subtract
+}
+})
