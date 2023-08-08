41d0edf4f9c127915eb0e923e8a8b96b00d8feb6
KabongeHajarah <kabongehajarah@gmail.com>
2023-08-08 20:50:06 +0400

    new

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { ReactDOM } from 'react-dom'
+    import React from 'react'
+    import { ReactDOM } from 'react-dom'
 
-const jsxElement=<h1>This is a JSX element</h1>
-const rootElement=document.getElementById('root')
-ReactDOM.render(jsxElement,rootElement)
+    const jsxElement=<h1>This is a JSX element</h1>
+    const rootElement=document.getElementById('root')
+    ReactDOM.render(jsxElement,rootElement)
