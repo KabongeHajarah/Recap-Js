85817f6e19f416ea94bc1dd27023d785d46a55ee
KabongeHajarah <kabongehajarah@gmail.com>
2023-08-08 20:49:38 +0400

    new

--- function.js
+++ function.js
@@ -1,38 +1,38 @@
-//Class
-class Person{
-    constructor (firstName,lastName,dob){
-        this.firstName=firstName;
-        this.lastName=lastName;
-        this.dob=new Date(dob);
-}
-getBirthYear(){
-    return this.dob.getFullYear();
-}
-getFullName(){
-    return  `${this.firstName} ${this.lastName}`
-}
-}
+    //Class
+    class Person{
+        constructor (firstName,lastName,dob){
+            this.firstName=firstName;
+            this.lastName=lastName;
+            this.dob=new Date(dob);
+    }
+    getBirthYear(){
+        return this.dob.getFullYear();
+    }
+    getFullName(){
+        return  `${this.firstName} ${this.lastName}`
+    }
+    }
 
 
-//Instantiate an object
-const person1=new Person('Bella','Kabonge','6-06-2002');
-console.log(person1);
-console.log(person1.dob.getFullYear());
-console.log(person1.getBirthYear());
-console.log(person1.getFullName());
+    //Instantiate an object
+    const person1=new Person('Bella','Kabonge','6-06-2002');
+    console.log(person1);
+    console.log(person1.dob.getFullYear());
+    console.log(person1.getBirthYear());
+    console.log(person1.getFullName());
 
-//Import from new.js
-//// to import the doSomeMath from the math.js with or without extension
-import doSomeMath from './new'
+    //Import from new.js
+    //// to import the doSomeMath from the math.js with or without extension
+    import doSomeMath from './new'
 
-//// to import the other modules
-// since these modules were not exported as default we have to desctructure
-import { addTwo,multiply,subtract } from './new';
+    //// to import the other modules
+    // since these modules were not exported as default we have to desctructure
+    import { addTwo,multiply,subtract } from './new';
 
-import * as everything from './new' //// to import everything remaining
-console.log(addTwo(5, 5))
-console.log(doSomeMath.addTwo(5, 5))
-console.log(everything)
-console.log(multiply(1,2));
-console.log(subtract(10,3));
-console.log(10,20);
+    import * as everything from './new' //// to import everything remaining
+    console.log(addTwo(5, 5))
+    console.log(doSomeMath.addTwo(5, 5))
+    console.log(everything)
+    console.log(multiply(1,2));
+    console.log(subtract(10,3));
+    console.log(10,20);
